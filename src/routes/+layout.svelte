69aef949564f526eae2405089651bcdbb28c8783
Sinda <sindabeqiri@Sindas-MacBook-Air.local>
2025-02-04 12:04:31 +0100

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+    let {children} = $props();
+    import "../style.css"
+</script>
+
+
+
+
+
+<header>
+    This is my  Projekt
+</header>
+
+<div class="body">
+
+
+<main>
+
+    {@render children()}
+
+</main>
+
+</div>
+
+
+<footer>
+    Built by : Ergit Beqiri
+</footer>
+
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+
+
+
+
+<script>
+    let message = $state("Write something here");
+</script>
+
+<div class="container">
+
+
+
+<h1 class="message">{message}</h1>
+
+<input type="text" bind:value={message}>
+
+</div>
